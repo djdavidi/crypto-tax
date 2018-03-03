9c616aeb11b0cc6fd8cb166ee001657f76baaa9e
[David Scherban] <davidscherban@gmail.com>
2018-03-02 22:50:02 -0500

    add global css file, create footer and header file, delete vue logo, and get the basics of the tree display

--- src/components/common/TreeDisplay.vue
+++ src/components/common/TreeDisplay.vue
@@ -4,33 +4,46 @@ with each exchange being a parent node and withdrawals, deposits, trades, and Al
 
 <template>
   <div class="tree-display">
-	Hello {{accounts[0]}}
+  	<div class="tree-display__tabs">
+  		<span v-for="tab in tabs" class="tree-display__tabs__tab"
+		@click="currentTab = tab"
+		:class="{'tree-display__tabs__tab--active': 'tab === currentTab'}">{{tab}}</span>
+  	</div>
+	<div class="tree-display__accounts">
+		<div @click="currentAccount = account" v-for="account in accounts" >{{account.name}}
+		<!-- Have click on here to remove -->
+		</div>
+		<div> Add</div>
+	</div>
+	<div class="tree-display__content">
+		Table component goes here
+		takes props of the fields depending on tab
+		object mapping and then displays
+		Give table pagination so don't have to render all 
+		those rows, let type in number into input
+	</div>
+	<div class="tree-display__summary">
+		summary thing
+	</div>
   </div>
 </template>
 
 <script>
+// Could pull this out and make prop
+// more reusable
+const tabs = ["Deposits", "Withdrawals", "Trades", "All"]
 import {mapState} from "vuex"
 export default {
   name: 'TreeDisplay',
   props: [],
   data () {
     return {
-      
+    	currentTab: "",
+    	currentAccount: "",
+    	tabs: tabs
     }
   },
   methods: {
-    calculateTotalGains() {
-
-    },
-    calculateTaxOnEvent() {
-
-    },
-    calculateFeeOnEvent() {
-      // trade
-      // Base pair before diff base pair after
-      // Base pair price per share * num of shares
-      // diff of these 2
-    }
   },
   computed: {
   	...mapState(["accounts"])
@@ -41,5 +54,45 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.tree-display {
+	width: calc(100% - 50px);
+	margin-left: 25px;
+	margin-right: 25px;
+	display: grid;
+	/*col or row start thing*/
+	grid-template-columns: 1fr 5fr 1.5fr;
+	grid-template-rows: auto;
+	grid-template-areas: 
+	"tabs tabs tabs"
+	"accounts content summary";
+}
 
+.tree-display .tree-display__tabs {
+	display: flex;
+	flex-direction: row;
+	grid-area: tabs;
+	/*width: 100%;*/
+}
+.tree-display__tabs__tab {
+	border: 1px;
+}
+
+.tree-display__accounts {
+	grid-area: accounts;
+}
+
+.tree-display__tabs__tab--active {
+	background: #CECECE;
+	color: #FFF;
+}
+
+.tree-account .tree-account__content {
+	background: #CECECE;
+	grid-area: content;
+	width: 100%;
+}
+
+.tree-display__summary {
+	grid-area: summary;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,25 @@ import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
 Vue.use(Vuex);
+// use vuex persisted state npm to store when close browser
+// so no need for backend - allow toggle - default off?
 const store = new Vuex.Store({
 // accounts are name and addresses(since exchanges have multiple)
 // {name: "binance", addresses[]}
 // and attached withdrawal, deposit, trades
   state: {
-    accounts: ["test account"]
+    accounts: [{name:"test account1"},{name:"test account2"}, {name:"test account3"}]
   },
   actions: {
     UPDATE_ACCOUNT: ({ commit }, updatedAccount, index) => {
-
-    	commit("SET_ITEMS", { account: updatedAccount, index:index})
+    	commit("UPDATE_ACCOUNT", { account: updatedAccount, index})
     },
     ADD_ACCOUNT: ({ commit }, newAccount) => {
-    	commit("SET_ITEMS", { account: newAccount})
+    	commit("ADD_ACCOUNT", { account: newAccount})
     },
+    REMOVE_ACCOUNT: ({ commit }, index) => {
+    	commit("REMOVE_ACCOUNT", {index})
+    }
   },
   mutations: {
     UPDATE_ACCOUNT: (state, { account, index }) => {
@@ -27,6 +31,9 @@ const store = new Vuex.Store({
     },
     ADD_ACCOUNT: (state, { account }) => {
       state.accounts.push(account)
+    },
+    REMOVE_ACCOUNT: ({ commit }, index) => {
+    	state.accounts.splice(index, 1)
     }
   },
   getters: {
